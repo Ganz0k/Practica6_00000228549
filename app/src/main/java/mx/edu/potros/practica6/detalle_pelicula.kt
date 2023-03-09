2e52d1ff6779afc09c32c68016fb3b6050de677a
Ganz0k <luisgonzalocervantes@gmail.com>
2023-03-09 12:10:40 -0700

    Practica 7

--- app/src/main/java/mx/edu/potros/practica6/detalle_pelicula.kt
+++ app/src/main/java/mx/edu/potros/practica6/detalle_pelicula.kt
@@ -1,7 +1,9 @@
 package mx.edu.potros.practica6
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 
@@ -14,6 +16,8 @@ class detalle_pelicula : AppCompatActivity() {
         val iv_pelicula_imagen: ImageView = findViewById(R.id.iv_pelicula_imagen)
         val tv_nombre_pelicula: TextView = findViewById(R.id.tv_nombre_pelicula)
         val tv_pelicula_desc: TextView = findViewById(R.id.tv_pelicula_desc)
+        val tv_seatLeft: TextView = findViewById(R.id.seatLeft)
+        val btnBuyTickers: Button = findViewById(R.id.buyTickets)
 
         val bundle = intent.extras
 
@@ -21,6 +25,14 @@ class detalle_pelicula : AppCompatActivity() {
             iv_pelicula_imagen.setImageResource(bundle.getInt("header"))
             tv_nombre_pelicula.setText(bundle.getString("nombre"))
             tv_pelicula_desc.setText(bundle.getString("sinopsis"))
+            tv_seatLeft.setText((bundle.getInt("numberSeats").toString()) + " seats available")
+        }
+
+        btnBuyTickers.setOnClickListener {
+            var intent = Intent(this, SeatSelection::class.java)
+            intent.putExtra("name", tv_nombre_pelicula.text)
+            intent.putExtra("id", -1)
+            startActivity(intent)
         }
     }
 }
